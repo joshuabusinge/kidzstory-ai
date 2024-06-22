6fd142018d512b0f5bb2d72e2e63518aead5739a
JOSHUA BUSINGE <joshuabujo9@gmail.com>
2024-06-22 17:20:19 +0300

    added the StoryWriter Component, Logo, Button With a link to the stories

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,25 @@
 import Image from "next/image";
+import Logo from "@/images/logo.png";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import StoryWriter from "@/components/StoryWriter";
 
 export default function Home() {
   return (
-    <main>
-      <h1>AI Agent GPTScript build</h1>
+    <main className="flex-1 flex flex-col">
+      <section className=" flex-1 grid grid-cols-1 lg:grid-cols-2">
+        <div className="bg-purple-500 flex flex-col space-y-5 justify-center items-center order-1 lg:-order-1 pb-10">
+          <Image src={Logo} height={250} alt="Logo" />
+
+          <Button asChild className="px-20 bg-purple-700 p-10 text-xl">
+            <Link href="/stories">Explore Story Library</Link>
+          </Button>
+        </div>
+
+        {/* StoryWriter */}
+
+        <StoryWriter />
+      </section>
     </main>
   );
 }

7d331fa486242ed0b6357775d92d0937c55de196
JOSHUA BUSINGE <joshuabujo9@gmail.com>
2024-06-24 19:23:45 +0300

    Created to decoded the stories in the pages

--- app/stories/page.tsx
+++ app/stories/page.tsx
@@ -4,11 +4,46 @@ import { BookOpen } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
+//server side render this page
+export const revalidate = 0;
+
 function Stories() {
   const stories: Story[] = getAllStories();
 
-  console.log(stories);
-  return <div> Stories</div>;
+  return (
+    <div className="p-10 max-w-7xl mx-auto">
+      {stories.length === 0 && <p>No stories found.</p>}
+
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5">
+        {stories.map((story) => (
+          <Link
+            href={`/stories/${story.story}`}
+            key={story.story}
+            className="border rounded-lg cursor-pointer hover:shadow-lg hover:border-purple-500 transition-all duration-300"
+          >
+            <div className="relative">
+              <p className="absolute flex items-center top-0 right-0 bg-white text-purple-500 font-bold p-3 rounded-lg m-2 text-sm">
+                <BookOpen className="w-4 h-4 mr-1" />
+                {story.pages.length === 1
+                  ? `${story.pages.length} Page`
+                  : `${story.pages.length} Pages`}
+              </p>
+              <Image
+                src={story.pages[1].png}
+                alt={story.story}
+                width={500}
+                height={500}
+                className="w-full object-contain rounded-t-lg"
+              />
+              <h2 className="text-lg p-5 first-letter:text-3xl font-light text-center">
+                {story.story}
+              </h2>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Stories;

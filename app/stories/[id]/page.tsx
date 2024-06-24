f3ece76a3f6db08060dbf3ca02118a7474d0f2ba
JOSHUA BUSINGE <joshuabujo9@gmail.com>
2024-06-24 21:51:18 +0300

    added return paths

--- app/stories/[id]/page.tsx
+++ app/stories/[id]/page.tsx
@@ -27,11 +27,14 @@ export default StoryPage;
 export async function generateStaticParams() {
   const stories = getAllStories();
 
-  //Explanation: We need to return an array of objects with the id property set to the story id. This will generate a static page for each story.
+  //Return an array of objects with the id property set to the story id.
+  //This will generate a static page for each story.
 
   //Example: [{id: "story-1"}, {id: "story-2"}, ...]
 
   const paths = stories.map((story) => ({
-    id: story.story,
+    params: { id: story.story },
   }));
+
+  return paths;
 }

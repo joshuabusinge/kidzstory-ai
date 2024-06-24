eb9423049c55abdc55d302f1d9af95d88b89c17f
JOSHUA BUSINGE <joshuabujo9@gmail.com>
2024-06-24 21:50:14 +0300

    added the toaster using the sonner ui from shad cn

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import { Toaster } from "sonner";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -21,6 +22,7 @@ export default function RootLayout({
         {children}
 
         {/* Toaster */}
+        <Toaster duration={8000} position="bottom-left" />
       </body>
     </html>
   );
